ede6a1eca8bfc17166e1dd666ef9f5d5527c8d2c
Caleb Allen <caleb@caleballen.com>
2018-07-09 16:09:58 -0700

    Removed DB Password from internet XD

--- db_connect.php
+++ db_connect.php
@@ -1,16 +1,9 @@
 <?php
 function connectToDatabase(){
-    if(!empty($_SERVER['RDS_HOSTNAME'])){
-        $dbhost = $_SERVER['RDS_HOSTNAME'];
-        $dbport = $_SERVER['RDS_PORT'];
-        $username = $_SERVER['RDS_USERNAME'];
-        $password = $_SERVER['RDS_PASSWORD'];
-    } else {
-        $dbhost = 'aaarl7tfabntmd.cvbl6vddyfbo.us-west-1.rds.amazonaws.com';
-        $dbport = '3306';
-        $username = 'nemo';
-        $password = 'BR94SHHvG5sM';
-    }
+    $dbhost = $_SERVER['RDS_HOSTNAME'];
+    $dbport = $_SERVER['RDS_PORT'];
+    $username = $_SERVER['RDS_USERNAME'];
+    $password = $_SERVER['RDS_PASSWORD'];
     $dbname = 'spotter';//$_SERVER['RDS_DB_NAME'];
     $charset = 'utf8' ;
 
