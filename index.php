bfc9ef5a4460b56bb53589585fa920dc29e9adcc
Caleb Allen <caleb@caleballen.com>
2018-07-12 09:45:11 -0700

    Adding basic Bootstrap Functionality and Template

--- index.php
+++ index.php
@@ -1,88 +1,167 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-  <style>
-body{
-  background-color: #181616;
-}
 
-hr {
-    display: block;
-    height: 1px;
-    border: 0;
-    border-top: 1px solid #595151;
-    margin: 1em 0;
-    padding: 0;
-}
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-.container{
-  width: 375px;
-  height: 667px;
-  background-color: #f9f0f0;
-  border: 2px solid #595151;
-  margin-left: auto;
-  margin-right: auto;
-  margin-top: 30px;
-  position: static;
-  color: #181616;
-  padding-top: 10px;
-    font-family: ‘Trebuchet MS’, Helvetica, sans-serif;
-      text-align: center;
-}
+    <title>SB Admin 2 - Bootstrap Admin Theme</title>
 
-.link{
-  min-height: 30px;
-  width: 300px;
-  border: 2px dashed #181616;
-  background-color: #f9f0f0;
-  margin-left: auto;
-  margin-right: auto;
-  margin-top: 30px;
-  margin-top: 10px;
-  position: relative;
-  vertical-align: middle;
-  font-size: 24px;
-}
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-div.link:hover{
-  min-height: 30px;
-  width: 300px;
-  border: 2px dashed #f9f0f0;
-  color: #f9f0f0;
-  background-color: #181616;
-  margin-left: auto;
-  margin-right: auto;
-  margin-top: 30px;
-  margin-top: 10px;
-  position: relative;
-  vertical-align: middle;
-  font-size: 24px;
-}
-a{
-  text-decoration: none;
-  color: black;
-}
-</style>
+    <!-- MetisMenu CSS -->
+    <link href="vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Morris Charts CSS -->
+    <link href="vendor/morrisjs/morris.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <style>
+      /* Set the size of the div element that contains the map */
+     #map {
+       height: 600px;  /* The height is 400 pixels */
+       width: 100%;  /* The width is the width of the web page */
+      }
+
+    </style>
 
 </head>
 
-	<body>
-	<div class="container">
-	  <h2>Nautilus Group's Spotter App</h2>
-	  <hr></hr>
-	  <h3>Database Check</h3>
-		<a href="db_test.php"><div class="link">Click here to verify database details.</div></a>
-	  <hr></hr>
-	</div>
+<body>
+
+    <div id="wrapper">
+
+        <?php 
+        //Allows ud to edit the navigation bar one time for all website pages.
+        require('includes/nav_bar.php') 
+        ?>
+
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Spottr, the photo location scouting app!</h1>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <i class="fa fa-photo fa-fw"></i> Photos in this region
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                                <img src="images/01.jpg" class="img-thumbnail" alt="Image1">
+                                <img src="images/02.jpg" class="img-thumbnail" alt="Image1">
+                                <img src="images/03.jpg" class="img-thumbnail" alt="Image1">
+                                <img src="images/04.jpg" class="img-thumbnail" alt="Image1">
+                                <img src="images/05.jpg" class="img-thumbnail" alt="Image1">
+
+                            <a href="#" class="btn btn-default btn-block">View All Alerts</a>
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+
+                </div>
+                <!-- /.col-lg-4 -->
+
+                <div class="col-lg-8">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <i class="fa fa-map-marker fa-fw"></i>Spotted Locations
+                            <div class="pull-right">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown">
+                                        Actions
+                                        <span class="caret"></span>
+                                    </button>
+                                    <ul class="dropdown-menu pull-right" role="menu">
+                                        <li><a href="#">Action</a>
+                                        </li>
+                                        <li><a href="#">Another action</a>
+                                        </li>
+                                        <li><a href="#">Something else here</a>
+                                        </li>
+                                        <li class="divider"></li>
+                                        <li><a href="#">Separated link</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <div id="map"></div>
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+
+                </div>
+                <!-- /.col-lg-8 -->
+
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="vendor/metisMenu/metisMenu.min.js"></script>
 
 
+    <!-- Custom Theme JavaScript -->
+    <script src="dist/js/sb-admin-2.js"></script>
 
+       <script>
+// Initialize and add the map
+function initMap() {
+ // The location of Uluru used Nicaragua but we can change
+ var uluru = {lat: 12.112949, lng: -86.281262};
+ // The map, centered at Uluru
+ console.log('got here');
+ var map = new google.maps.Map(
 
+     document.getElementById('map'), {zoom: 4, center: uluru});
+ // The marker, positioned at Uluru
+ var marker = new google.maps.Marker({position: uluru, map: map});
+}
+   </script>
+   <!--Load the API from the specified URL
+   * The async attribute allows the browser to render the page while the API loads
+   * The key parameter will contain your own API key (which is not needed for this tutorial) I setup a key in geocoding api
+   * The callback parameter executes the initMap() function
+   -->
+   <script async defer
+   src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAxnMU9bIct86r3W4-rJ22Sirsli0U3uH4&callback=initMap">
+   </script>
 
-	</body>
-
-
-
-
-
+</body>
 
 </html>
