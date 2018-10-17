29be46baccf99d60caefcf18b01cd2017c245e39
Caleb Allen <CAllen@Atwater.local>
2018-10-17 07:03:51 -0700

    Fixing Dependency

--- index.php
+++ index.php
@@ -476,8 +476,8 @@
       <!-- Bootstrap Core JavaScript -->
       <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
 
-      <!-- Metis Menu Plugin JavaScript 
-      <script src="vendor/metisMenu/metisMenu.min.js"></script>-->
+      <!-- Metis Menu Plugin JavaScript -->
+      <script src="vendor/metisMenu/metisMenu.min.js"></script>
 
 
       <!-- Custom Theme JavaScript -->
